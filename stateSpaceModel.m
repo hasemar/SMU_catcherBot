d6f6ac67eabd356ac1e390b20508bc18a7fcd5b8
hasemar <hasemar@gmail.com>
2017-03-08 11:39:23 -0800

    motorConstant and Response
    Put in motor constant and K value. Also tried to get a response with an
    initial condition of 2.445 m/s (fall from 1ft high).  I dont think my
    response is correct I need picone to help

--- stateSpaceModel.m
+++ stateSpaceModel.m
@@ -3,38 +3,35 @@ clear; close all
 
 %% define vars
 % motor parameters
-    Kt = 1;         % torque constant = 2NBlr
-    Kv = 1;         % back-emf constant (incase have this instead)
-    L = 1;          % H motor inductance
-    r = 1000;       % ohms  motor resistance
+    Kt = 6.66;       % Nm/A torque constant based off BSM-50N-275 ABB servo
+    %Kv = 1;         % back-emf constant (incase have this instead)
+    L = .0332;       % H motor inductance
+    r = 16;          % ohms  motor resistance
 % physical parameters    
-    m1 = 10;        % kg mass of platform
-    m2 = 3;         % kg mass of puck
-    k = 1000;       % N/m mechanical spring element
-    b1 = 1000;      % Ns/m
-    J = 1;          % kgm^2 moment of inertial
-    b2 = 1000;      % Ns/m bearing friction
-    p1 = 5;         % cm  pulley 1
-    p2 = 5;         % cm pulley 2
-    pRatio = p2/p1;
+    m1 = 5;          % kg mass of platform
+    m2 = 3;          % kg mass of puck
+    k = 18500;       % N/m mechanical spring element
+    b1 = 1000;       % Ns/m
+    J = 11240;       % kgm^2 moment of inertial
+    b2 = 60*.002;       % Ns/m bearing friction
+    rWheel = 2.5;    % cm  drive wheel radius
 % transformer
-    TFbelt = pRatio;     % transformer translation to rotation
-    TFmotor = Kt;    % transformer rotation to electrical
+    TFrp = rWheel;     % transformer translation to rotation
+    TFmotor = Kt;      % transformer rotation to electrical
 
 % stuff
-Vs = 12;        % volts voltage supply
-TFelement = (b1*TFbelt^2*TFmotor)/(J*TFmotor*m2*TFbelt^2*TFmotor); % calculating elements in A
-t = 0:.001:1;  % time array
+TFelement = (b1*TFrp^2*TFmotor)/(J*TFmotor*m2*TFrp^2*TFmotor); % calculating elements in A
+t = 0:.001:3;  % time array
 
 %% define matricies
 x0 = [...
-    0;...
-    0;...
-    0;...
-    0;];
+    2.445;... % Vm1
+    0;... % Vm2
+    0;... % Fk
+    0;];  %iL
 A = [...
     -b1/m1, b1/m1, -1/m1,0;...
-    TFelement, -TFelement, TFelement, TFelement/(TFbelt*TFmotor);...
+    TFelement, -TFelement, TFelement, TFelement/(TFrp*TFmotor);...
     k, -k,0,0;...
     0,0,0,-r/L;];
     
@@ -50,16 +47,24 @@ D = 0;
 sys = ss(A,B,C,D);
 %sys.InputName = 'Vs';
 %sys.OutputName = {'Vm1';'Vm2'};
-F1 = impulse(sys,t);
+u = NaN*ones(1,length(t));
+for j = 1:length(t)
+    u(j) = 0;
+end
+y = lsim(sys,u,t,x0);
 
 %% plot things
 figure
-plot(t,F1);
-title('Impulse Response');
+plot(t,y);
+%impulse(plant,t);
+hold on
+grid on
+%plot(t,u);
+title('Response');
 xlabel('time (s)')
-ylabel('velocity of m1 (m/s)')
+ylabel('velocity of puck (m/s)')
 
-figure(2)
-rlocus(sys);
+%figure(2)
+%rlocus(plant);
 
 
